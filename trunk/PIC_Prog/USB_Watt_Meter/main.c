cb4f032f6a321df5ede8a84d387185a290573198
bant62@gmail.com <bant62@gmail.com@5cd43b6b-74d2-37a4-b8e0-dbc222e4b626>
2013-01-22 23:00:45 +0000

    PIC Prog: USB Watt Meter ソースタイトルを修正

--- trunk/PIC_Prog/USB_Watt_Meter/main.c
+++ trunk/PIC_Prog/USB_Watt_Meter/main.c
@@ -1,4 +1,4 @@
-//=============================================================================
+//==========================================================================
 // File Name    : main.c
 //
 // Title        : USB 充電電力計ファームウェア
@@ -13,8 +13,22 @@
 // -----------  ----------- -----------------------
 // 2013/01/01   ばんと      開発開始
 // 2013/01/19   ばんと      Ver0.1完了
-// 2012/11/25   ばんと      AVR toolchain Ver3.4.1.1195に対応
-//=============================================================================
+//==========================================================================
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//==========================================================================
 
 /** ファイルインクルード **************************/
 #include "USB/usb.h"
